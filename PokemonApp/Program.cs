94492abfb1084f746898cae500fc34590e4fdb73
Calvin Tan <calvintan607@gmail.com>
2022-10-27 15:06:21 -0700

    fixed a mistake where computer was taking players roll instead of its own

--- PokemonApp/Program.cs
+++ PokemonApp/Program.cs
@@ -22,11 +22,10 @@ namespace PokemonApp{
                 Console.WriteLine("Computer has " + computer.getHealth()+ " left");
 
                 //Computers turn
-                int computersInput = computerRolls.Next(1,5);
+                int computersInput = computerRolls.Next(0,4);
                 Console.WriteLine(computersInput);
-                
-                Console.WriteLine("Computer's damage dealt: " + computer.attack(computersInput-1));
-                player.setHealth(player.getHealth()-computer.attack(input));
+                Console.WriteLine("Computer used "+computer.getMoveset(computersInput).getAbilityName() + "! " +" Computer's damage dealt: " + computer.attack(computersInput));
+                player.setHealth(player.getHealth()-computer.attack(computersInput));
                 Console.WriteLine("Player has " + player.getHealth() + " left");
 
         }
@@ -36,14 +35,14 @@ namespace PokemonApp{
             Abilities[] movesetA = new Abilities[4]{
                 new Abilities("flamethrower",50,20),
                 new Abilities("tackle",20,30),
-                new Abilities("splash",0,10),
+                new Abilities("fly",20,10),
                 new Abilities("bite",30,50),
             };
             Pokemon pikachu = new Pokemon("pikachu",21,"rat",100,movesetA);
             Abilities[] movesetB = new Abilities[4]{
                 new Abilities("flamethrower",50,20),
                 new Abilities("tackle",20,30),
-                new Abilities("splash",0,10),
+                new Abilities("surf",30,10),
                 new Abilities("bite",30,50),
             };
             Pokemon magikarp = new Pokemon("magikarp",22,"fish",200,movesetB);
